d84b681b46712fb682875738df6f2834d21ee178
sheroz <sheroz@dreamlogic.io>
2023-07-27 19:31:10 +0300

    added SHA-3-256 sample

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
 use openssl::sha::sha256;
+use openssl::hash::{Hasher, MessageDigest};
+
 
 fn main() {
-    let txt = "Hello world!";
+    println!("\nOpenSSL samples\n");
+
+    let txt = "abc";
     let hash = sha256(txt.as_bytes());
     let digest = hex::encode(hash);    
-    assert_eq!(digest, "c0535e4be2b79ffd93291305436bf889314e4a3faec05ecffcbb7df31ad9e51a");
-    println!("\nOpenSSL: SHA256 hash for {}\n{}\n", txt, digest);
+    assert_eq!(digest, "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad");
+    println!("SHA-256 for {}\n{}\n", txt, digest);
+
+    let mut hasher = Hasher::new(MessageDigest::sha3_256()).unwrap();
+    hasher.update(txt.as_bytes()).unwrap();
+    let hash = hasher.finish().unwrap();
+    let digest = hex::encode(hash);    
+    assert_eq!(digest, "3a985da74fe225b2045c172d6bd390bd855f086e3e9d525b46bfe24511431532");
+    println!("SHA-3-256 hash for {}\n{}\n", txt, digest);
 }
